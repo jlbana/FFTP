10ba0134d47e2e77daca9fa941960c06b63c2a36
jlbana <76172758+jlbana@users.noreply.github.com>
2020-12-18 09:40:09 +0000

    Authentication checks

--- src/fftp_auth.c
+++ src/fftp_auth.c
@@ -1,6 +1,23 @@
 #include "fftp_auth.h"
 
+#include <stdio.h>
+#include <crypt.h>
+#include <shadow.h>
+#include <string.h>
+
 bool fftp_auth_verify(char *username, char *password)
 {
-	return true;
+	struct spwd *entry = getspnam(username);
+
+	if (entry == NULL)
+	{
+		return false;
+	}
+
+	char *original = entry->sp_pwdp, *result;
+	result = crypt(password, original);
+
+	if (! strcmp(result, original))
+		return true;
+	return false;
 }
