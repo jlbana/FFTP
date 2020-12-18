8d65a6974a482107abd764a2016d4d4ebccebccd
jlbana <76172758+jlbana@users.noreply.github.com>
2020-12-18 14:05:08 +0000

    Brrr

--- src/fftp_run.c
+++ src/fftp_run.c
@@ -12,17 +12,17 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 
 	switch(key)
 	{
-		case 'i':
-			arguments->ip = arg;
-			break;
-		case 'p':
-		{
-			unsigned port = atoi(arg);
-			arguments->port = port;
-			break;
-		}
-		default:
-			return ARGP_ERR_UNKNOWN;
+	case 'i':
+		arguments->ip = arg;
+		break;
+	case 'p':
+	{
+		unsigned port = atoi(arg);
+		arguments->port = port;
+		break;
+	}
+	default:
+		return ARGP_ERR_UNKNOWN;
 	}
 
 	return 0;
